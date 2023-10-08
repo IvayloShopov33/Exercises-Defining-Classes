f88b0e02c19cae1800beb4f04c9261df0ceaf292
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-09 02:09:42 +0300

    Add files via upload

---
+++ 02. Creating Constructors/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person zeroArgumentPerson = new Person();
+            Person oneArgumentPerson = new Person(17);
+            Person twoArgumentsPerson = new Person("Ivaylo", 17);
+        }
+    }
+}
