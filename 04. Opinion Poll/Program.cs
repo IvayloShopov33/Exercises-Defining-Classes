9cd3e222ba010037b8e7da37ba6485983f4d8f1e
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-10 00:28:12 +0300

    Add files via upload

---
+++ 04. Opinion Poll/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DefiningClasses
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= peopleCount; i++)
+            {
+                string[] personDetails = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = personDetails[0];
+                int age = int.Parse(personDetails[1]);
+                Person person = new Person(name, age);
+                people.Add(person);
+            }
+
+            people = people.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+            foreach (Person human in people)
+            {
+                Console.WriteLine($"{human.Name} - {human.Age}");
+            }
+        }
+    }
+}
