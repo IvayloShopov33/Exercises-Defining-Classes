cf31fb291c4eac52afc2208d4e22deca1a42526d
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-10 00:27:08 +0300

    Add files via upload

---
+++ 03. Oldest Family Member/Family.cs
@@ -0,0 +1,25 @@
+﻿namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            this.People = new List<Person>();
+        }
+
+        public List<Person> People { get { return people; } set { people = value; } }
+
+        public void AddMember(Person member)
+        {
+            this.People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            int maximumAge = this.People.Max(p => p.Age);
+            return this.People.FirstOrDefault(x => x.Age == maximumAge);
+        }
+    }
+}
---
+++ 03. Oldest Family Member/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+            int familyMembersCount = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= familyMembersCount; i++)
+            {
+                string[] personDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = personDetails[0];
+                int age = int.Parse(personDetails[1]);
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Person oldestFamilyMember = family.GetOldestMember();
+            Console.WriteLine($"{oldestFamilyMember.Name} {oldestFamilyMember.Age}");
+        }
+    }
+}
