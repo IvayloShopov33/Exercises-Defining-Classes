377da48e1a71cbcae37857a5a370d8b69b45e7e8
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-11 00:07:53 +0300

    Add files via upload

---
+++ 05. Date Modifier/DateModifier.cs
@@ -0,0 +1,13 @@
+﻿namespace _05._Date_Modifier
+{
+    public class DateModifier
+    {
+        public static int CalculateDateDifferenceInDays(string firstDate, string secondDate)
+        {
+            DateTime dateOne = DateTime.Parse(firstDate);
+            DateTime dateTwo = DateTime.Parse(secondDate);
+            int daysDifference = Math.Abs((dateOne - dateTwo).Days);
+            return daysDifference;
+        }
+    }
+}
---
+++ 05. Date Modifier/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _05._Date_Modifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+            int daysDifference = DateModifier.CalculateDateDifferenceInDays(firstDate, secondDate);
+            Console.WriteLine(daysDifference);
+        }
+    }
+}
