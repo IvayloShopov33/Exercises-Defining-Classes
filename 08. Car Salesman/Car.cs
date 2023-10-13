463da52be068a9d5c2c5c79714fde148fe4878e0
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-14 01:21:09 +0300

    Add files via upload

---
+++ 08. Car Salesman/Car.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+
+namespace _08._Car_Salesman
+{
+    public class Car
+    {
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+        }
+
+        public Car(string model, Engine engine, int weight) : this(model, engine)
+        {
+            this.Weight = weight;
+        }
+
+        public Car(string model, Engine engine, int weight, string color) : this(model, engine, weight)
+        {
+            this.Color = color;
+        }
+
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public int Weight { get; set; }
+
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            output.AppendLine(this.Model + ":");
+            output.AppendLine($"  {this.Engine.Model}:");
+            output.AppendLine($"    Power: {this.Engine.Power}");
+
+            if (this.Engine.Displacement != 0)
+            {
+                output.AppendLine($"    Displacement: {this.Engine.Displacement}");
+            }
+            else
+            {
+                output.AppendLine($"    Displacement: n/a");
+            }
+
+            if (this.Engine.Efficiency != null)
+            {
+                output.AppendLine($"    Efficiency: {this.Engine.Efficiency}");
+            }
+            else
+            {
+                output.AppendLine($"    Efficiency: n/a");
+            }
+
+            if (this.Weight != 0)
+            {
+                output.AppendLine($"  Weight: {this.Weight}");
+            }
+            else
+            {
+                output.AppendLine($"  Weight: n/a");
+            }
+
+            if (this.Color != null)
+            {
+                output.Append($"  Color: {this.Color}");
+            }
+            else
+            {
+                output.Append($"  Color: n/a");
+            }
+
+            return output.ToString();
+        }
+    }
+}
---
+++ 08. Car Salesman/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace _08._Car_Salesman
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Engine> engines = new List<Engine>();
+            int enginesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= enginesCount; i++)
+            {
+                string[] engineDetails = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string engineModel = engineDetails[0];
+                int enginePower = int.Parse(engineDetails[1]);
+                Engine engine = null;
+
+                if (engineDetails.Length == 2)
+                {
+                    engine = new Engine(engineModel, enginePower);
+                }
+                else if (engineDetails.Length == 3 && char.IsDigit(engineDetails[2][0]))
+                {
+                    int engineDisplacement = int.Parse(engineDetails[2]);
+                    engine = new Engine(engineModel, enginePower, engineDisplacement);
+                }
+                else if (engineDetails.Length == 3 && char.IsLetter(engineDetails[2][0]))
+                {
+                    int displacement = 0;
+                    string engineEfficiency = engineDetails[2];
+                    engine = new Engine(engineModel, enginePower, displacement, engineEfficiency);
+                }
+                else if (engineDetails.Length == 4)
+                {
+                    int engineDisplacement = int.Parse(engineDetails[2]);
+                    string engineEfficiency = engineDetails[3];
+                    engine = new Engine(engineModel, enginePower, engineDisplacement, engineEfficiency);
+                }
+
+                if (engine != null)
+                {
+                    engines.Add(engine);
+                }
+            }
+
+            List<Car> cars = new List<Car>();
+            int carsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= carsCount; i++)
+            {
+                string[] carDetails = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string carModel = carDetails[0];
+                string carEngine = carDetails[1];
+                Engine engine = engines.FirstOrDefault(x => x.Model == carEngine);
+                Car car = null;
+
+                if (carDetails.Length == 2)
+                {
+                    car = new Car(carModel, engine);
+                }
+                else if (carDetails.Length == 3 && char.IsDigit(carDetails[2][0]))
+                {
+                    int carWeight = int.Parse(carDetails[2]);
+                    car = new Car(carModel, engine, carWeight);
+                }
+                else if (carDetails.Length == 3 && char.IsLetter(carDetails[2][0]))
+                {
+                    int weight = 0;
+                    string carColor = carDetails[2];
+                    car = new Car(carModel, engine, weight, carColor);
+                }
+                else if (carDetails.Length == 4)
+                {
+                    int carWeight = int.Parse(carDetails[2]);
+                    string carColor = carDetails[3];
+                    car = new Car(carModel, engine, carWeight, carColor);
+                }
+
+                if (car != null)
+                {
+                    cars.Add(car);
+                }
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
