af345af450009afd0e62b3c15179157a20217cbe
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-16 01:28:10 +0300

    Add files via upload

---
+++ 10. SoftUni Parking/Parking.cs
@@ -0,0 +1,64 @@
+﻿namespace SoftUniParking
+{
+    public class Parking
+    {
+        private List<Car> cars;
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            this.Cars = new List<Car>();
+            this.capacity = capacity;
+        }
+
+        public List<Car> Cars { get { return cars; } set { cars = value; } }
+
+        public int Count
+            => this.Cars.Count;
+
+        public string AddCar(Car car)
+        {
+            if (this.Cars.Any(x => x.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (this.capacity == this.Cars.Count)
+            {
+                return "Parking is full!";
+            }
+
+            this.Cars.Add(car);
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (this.Cars.Any(x => x.RegistrationNumber == registrationNumber))
+            {
+                Car carToRemove = this.Cars.First(x => x.RegistrationNumber == registrationNumber);
+                this.Cars.Remove(carToRemove);
+                return $"Successfully removed {registrationNumber}";
+            }
+
+            return "Car with that registration number, doesn't exist!";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return this.Cars.First(x => x.RegistrationNumber == registrationNumber);
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (string registrationNumber in registrationNumbers)
+            {
+                if (this.Cars.Any(x => x.RegistrationNumber == registrationNumber))
+                {
+                    Car carToRemove = this.Cars.First(x => x.RegistrationNumber == registrationNumber);
+                    this.Cars.Remove(carToRemove);
+                }
+            }
+        }
+    }
+}
---
+++ 10. SoftUni Parking/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SoftUniParking
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car("Mazda", "Miata", 70, "CC1857BG");
+            Car car2 = new Car("Lamborghini", "Huracan", 600, "EB9999MN");
+            Console.WriteLine(car.ToString());
+
+            Parking parking = new Parking(5);
+            Console.WriteLine(parking.AddCar(car));
+            Console.WriteLine(parking.AddCar(car));
+            Console.WriteLine(parking.AddCar(car2));
+            Console.WriteLine(parking.GetCar("EB9999MN").ToString());
+            Console.WriteLine(parking.RemoveCar("EB9999MN"));
+
+            List<string> registrationNumbers = new List<string>
+            {
+                "CC1857BG"
+            };
+
+            parking.RemoveSetOfRegistrationNumber(registrationNumbers);
+            Console.WriteLine(parking.Count);
+
+        }
+    }
+}
